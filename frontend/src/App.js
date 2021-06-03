437af700a7a90a4940f031cccf88defc63892801
Vuralcan Asal <vuralcanasal@gmail.com>
2021-06-03 14:56:35 +0300

    Create first screen by using ract-bootstrao

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import { Container } from 'react-bootstrap';
 
 const App = () => {
   return (
-    <main>
-      <h1>
-        sadsadasd
-     </h1>
-    </main>
+    <>
+      <Header />
+      <main clasName='py-3'>
+        <Container>
+          <h1>Best shopping</h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
   );
 }
 
