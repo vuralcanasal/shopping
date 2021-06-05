ddeb81b3a801f1e1cd710f6b79e8e64854d5f876
Vuralcan Asal <vuralcanasal@gmail.com>
2021-06-05 17:56:34 +0300

    Connection between frontend and backend first step

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -2,9 +2,11 @@ import React from 'react'
 import { Card } from 'react-bootstrap'
 import Rating from './Rating'
 import { Link } from 'react-router-dom'
+import { useState } from 'react'
 
 
 const Product = ({ product }) => {
+
     return (
         <Card className='my-3 p-3 rounded'>
             <Link to={`/product/${product._id}`}>
--- frontend/src/views/HomeView.js
+++ frontend/src/views/HomeView.js
@@ -1,12 +1,22 @@
-import React from 'react'
-import products from '../products'
+import React, { useState, useEffect } from 'react'
 import Product from '../components/Product'
 import { Row, Col } from 'react-bootstrap'
+import axios from 'axios'
+
 
 const HomeView = () => {
+    const [products, setProducts] = useState([])
+    useEffect(() => {
+        const fetchProducts = async () => {
+            const { data } = await axios.get('/api/products')
+            setProducts(data)
+        }
+
+        fetchProducts()
+    }, [])
     return (
         <>
-            <h1>Latest Products</h1>
+            <h1>Best Products for YOU!!</h1>
             <Row>
                 {products.map(product => (
                     <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
