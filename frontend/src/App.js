f12f0085f66ce627a4c27b2c65fb9d2d53a4a9f1
Vuralcan Asal <vuralcanasal@gmail.com>
2021-06-05 11:50:57 +0300

    frontend issues main page and product page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,18 +2,22 @@ import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import { Container } from 'react-bootstrap';
+import HomeView from './views/HomeView';
+import ProductView from './views/ProductView';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
-      <main clasName='py-3'>
+      <main className='py-3'>
         <Container>
-          <h1>Best shopping</h1>
+          <Route path='/' component={HomeView} exact />
+          <Route path='/product/:id' component={ProductView} />
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 }
 
